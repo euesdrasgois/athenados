1d1e931884bccd0ba0104b5cd705f00703c7a632
Esdras Gois <43199901+euesdrasgois@users.noreply.github.com>
2020-06-26 12:12:52 -0300

    Portugues

--- app/index.php
+++ app/index.php
@@ -50,7 +50,7 @@
 
     </div>
     <div class="footer">
-    <a rel="license" href="http://creativecommons.org/licenses/by/3.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/3.0/80x15.png" /><br></a>Esse trabalho é licensiado pela <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Unported License</a>.
+    <a rel="license" href="http://creativecommons.org/licenses/by/3.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/3.0/80x15.png" /><br></a>Esse trabalho é licenciado pela <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Unported License</a>.
     </div> 
     <script>
         function openPage(url) {
