e1a6ed8a11fabd952636854ec67914baf0b4f501
Esdras Gois <43199901+euesdrasgois@users.noreply.github.com>
2020-06-26 22:45:31 -0300

    updt

--- app/matematica/p-1/index.php
+++ app/matematica/p-1/index.php
@@ -4,17 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Matemática Básica - Athenados</title>
     <link rel="stylesheet" type="text/css" href="../../css/assuntos.css"">    
     </head>
     <body>   
         <?php
         include '../../modules-html/header.php';
         ?>
-        <img class=" classes" src="../../src/images/subjects/matematica/p1/fracoes.png" alt="Frações">
-
+        <!-- ########################### -->        
+        <div class="content">
+            <img class=" assunto" src="../../src/images/subjects/matematica/p1/potenciacao.png" alt="Frações">
+            <img class="assunto" src="../../src/images/subjects/matematica/p1/radiciacao.png" alt="">
+        </div>
+        <!-- ########################### -->
         <?php
         include '../../modules-html/footer.php';
         ?>
     </body>
+
 </html>
---
+++ app/matematica/p-1/potenciacao.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Potenciação - Athenados</title>
+</head>
+
+<body>
+    <?php
+    include '../../modules-html/header.php'
+    ?>
+
+    <?php
+    include '../../modules-html/footer.php'
+    ?>
+</body>
+
+</html>
