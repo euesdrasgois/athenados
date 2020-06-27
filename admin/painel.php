f4f8b8e504beb01c4fed27c1318e3b84890dac7c
Esdras Gois <43199901+euesdrasgois@users.noreply.github.com>
2020-06-27 14:38:16 -0300

    download

--- admin/painel.php
+++ admin/painel.php
@@ -21,22 +21,22 @@ include 'verifica_login.php';
     $mensagem = null;
     if (isset($_POST['enviar'])) :
         $formatosPermitidos = ["apk"];
-        $extensao = pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION);        
-        if(in_array($extensao, $formatosPermitidos)){
+        $extensao = pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION);
+        if (in_array($extensao, $formatosPermitidos)) {
             $pasta = "../download/";
             $temporario = $_FILES['arquivo']['tmp_name'];
-            $novonome = "Athenados".".$extensao";
-            if(move_uploaded_file($temporario, $pasta.$novonome)){
+            $novonome = "Athenados" . ".$extensao";
+            if (move_uploaded_file($temporario, $pasta . $novonome)) {
                 $mensagem = "Upload feito com sucesso!";
                 include 'conexao.php';
                 $query = "INSERT INTO `app_info` (`id`, `version`, `date`) VALUES (NULL, '{$_POST['version']}', CURRENT_DATE());";
                 $result = mysqli_query($conexao, $query);
-            }else {
+            } else {
                 $mensagem = "Erro. Não foi possível completar o upload.";
             }
         } else {
             $mensagem = "Formato inválido!";
-        }       
+        }
 
     endif;
     ?>
@@ -44,15 +44,15 @@ include 'verifica_login.php';
     <div class="content">
         <div id="versionUpdate" class="left">
             <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
-
+                <h2>Enviar Atualização</h2>
                 <label for="version">Nome da Versão: </label><input type="text" name="version" id="version"><br>
                 <input type="file" name="arquivo" id="arquivo"><br>
                 <input type="submit" value="Enviar" name="enviar" id="enviar">
             </form>
-            <?php    
-            if($mensagem){
+            <?php
+            if ($mensagem) {
                 echo $mensagem;
-            }    
+            }
             ?>
         </div>
     </div>
